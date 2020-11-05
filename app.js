6254a3706515f67c19a489e0fa98aabdf42556cc
dayanaferrer <dayanaferreira2@gmail.com>
2020-11-05 15:39:47 -0300

    app criado e validado

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/reprograma', {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+});
+
+let db = mongoose.connection;
+
+db.on('error', console.log.bind(console, 'connection error'));
+db.once('open', () => {
+  console.log('Conexão feita concluida sucesso!')
+});
+
+const clientes = require('./routes/clientesRoute');
+
+app.use(express.json());
+
+app.use('/', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  });
+
+app.use('/clientes', clientes);
+
+module.exports = app;
